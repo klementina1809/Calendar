bdfb8f670586e4491acd9d5f7c464d283bfbb205
klementina1809 <klementina.ua@gmail.com>
2024-03-19 22:00:58 +0100

    .

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -3,33 +3,29 @@ import Week from "./Week";
 
 function Month({ firstDay, days }) {
 	const days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-	const weeksNumber = Math.ceil((days + firstDay-1) / 7);
+	const weeksNumber = Math.ceil((days + firstDay - 1) / 7);
+
 	const getMonth = () => {
-		const month = [];
 		let currentDay = 1;
+		const month = [
+			<Week
+				key={0}
+				firstDay={firstDay}
+				firstNumber={currentDay}
+				days={days}
+			/>,
+		];
 
 		for (let i = 0; i < weeksNumber; i++) {
-			if (i === 0) {
-				month.push(
-					<Week
-						key={i}
-						firstDay={firstDay}
-						firstNumber={currentDay}
-						days={days}
-					/>
-				);
-				currentDay += 7;
-			} else {
-				month.push(
-					<Week
-						key={i}
-						firstDay={1}
-						firstNumber={currentDay}
-						days={days}
-					/>
-				);
-				currentDay += 7;
-			}
+			month.push(
+				<Week
+					key={i}
+					firstDay={1}
+					firstNumber={currentDay}
+					days={days}
+				/>
+			);
+			currentDay += 7;
 		}
 
 		return month;
